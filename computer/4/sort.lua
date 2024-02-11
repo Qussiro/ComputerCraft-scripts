0d3855965ef4d7765e5a41a617584a9a17e459fc
HappyMikki <dimazharov550@gmail.com>
2024-02-11 18:28:32 +0200

    commit tutel-work

--- computer/4/sort.lua
+++ computer/4/sort.lua
@@ -1,3 +1,8 @@
+local height = 2
+local width = 6
+local row = 0
+local col = 0
+
 function go_to_next_chest()
     if row < height - 1 then
         turtle.up()
@@ -5,6 +10,7 @@ function go_to_next_chest()
     else
         while row > 0 do
             turtle.down()
+            row = row - 1
         end
         turtle.turnLeft()
         turtle.forward()
@@ -13,13 +19,25 @@ function go_to_next_chest()
     end
 end
 
-local width = 2
-local height = 2
-local row = 0
-local col = 0
-
+local input_chest = peripheral.wrap("front")
+local need_to_sort = false
+while not need_to_sort do
+    local count = 0
+    for _ in pairs(input_chest.list()) do
+        count = count + 1
+    end
+    if count > 0 do
+        need_to_sort = true
+    end
+    sleep(5)
+end
+turtle.turnLeft()
+turtle.turnLeft()
+turtle.forward()
+turtle.forward()
+turtle.turnRight()
 local empty = false
-while ~empty do
+while not empty do
     local slot = 16
     while slot > 0 do
         turtle.select(slot)
@@ -37,11 +55,31 @@ while ~empty do
         slot = slot - 1
     end
 
+    empty = true
     slot = 16
     while slot > 0 do
-        if turtle.getItemCount(slot) > 0 do
+        if turtle.getItemCount(slot) > 0 then
             empty = false
             break
         end
+        slot = slot - 1
+    end
+    
+    if not empty then
+        go_to_next_chest()
     end
 end
+print("Done")
+while row > 0 do
+    turtle.down()
+    row = row - 1
+end
+if col > 0 then
+    turtle.turnRight()
+end
+while col > 0 do
+    turtle.forward()
+    col = col - 1
+end
+turtle.forward()
+turtle.forward()
