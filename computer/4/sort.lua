eb984387ebda0ea026ad7f326879f3eb46fed217
Sylphri <sylphri@gmail.com>
2024-02-11 18:06:08 +0200

    Turtle movement

--- computer/4/sort.lua
+++ computer/4/sort.lua
@@ -1,22 +1,15 @@
 function go_to_next_chest()
-    if col < width - 1 then
-        turtle.turnRight()
-        turtle.forward()
-        turtle.forward()
-        turtle.turnLeft()
-        col = col + 1
-    else
-        turtle.turnLeft()
-        local i = col
-        while i >= 0 do
-            turtle.forward()
-            turtle.forward()
-            i = i - 1
-        end
-        turtle.down()
-        turtle.turnRight()
-        col = 0
+    if row < height - 1 then
+        turtle.up()
         row = row + 1
+    else
+        while row > 0 do
+            turtle.down()
+        end
+        turtle.turnLeft()
+        turtle.forward()
+        turtle.turnRight()
+        col = col + 1
     end
 end
 
@@ -24,20 +17,31 @@ local width = 2
 local height = 2
 local row = 0
 local col = 0
-local count = 16
 
-while count > 0 do
-    turtle.select(count)
-    item = turtle.getItemDetail()
-    if item ~= nil then
-        local chest = peripheral.wrap("front")
-        local content = chest.list()
-        for _, chest_item in pairs(content) do
-            if chest_item.name == item.name then
-                turtle.drop()
-                break
+local empty = false
+while ~empty do
+    local slot = 16
+    while slot > 0 do
+        turtle.select(slot)
+        item = turtle.getItemDetail()
+        if item ~= nil then
+            local chest = peripheral.wrap("front")
+            local content = chest.list()
+            for _, chest_item in pairs(content) do
+                if chest_item.name == item.name then
+                    turtle.drop()
+                    break
+                end
             end
         end
+        slot = slot - 1
     end
-    count = count - 1
+
+    slot = 16
+    while slot > 0 do
+        if turtle.getItemCount(slot) > 0 do
+            empty = false
+            break
+        end
+    end
 end
