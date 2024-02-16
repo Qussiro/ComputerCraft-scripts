3b2dbbf6a38af332118d8f6b0fb5eb19bba4a5f0
HappyMikki <dimazharov550@gmail.com>
2024-02-16 17:34:07 +0200

    Fixed strange interaction between portable interface and "thing"

--- computer/7/tree_farm.lua
+++ computer/7/tree_farm.lua
@@ -2,20 +2,30 @@ while true do
     local farm_storage = peripheral.wrap("left")
     local wood_storage = peripheral.wrap("right")
     local sticks_storage = peripheral.wrap("back")
-   
+    
     for slot, item in pairs(farm_storage.list()) do
+        if next(farm_storage.list()) == nil then 
+            break
+        end
         if item.name == "minecraft:spruce_log" then
             local count = 0
             for _ in pairs(wood_storage.list()) do
                 count = count + 1
             end
             if count < wood_storage.size() then
+                if next(farm_storage.list()) == nil then 
+                    break
+                end
                 farm_storage.pushItems(peripheral.getName(wood_storage), slot)
                 print("Tranfered wood")
             end
         elseif item.name == "minecraft:stick" then
+            if next(farm_storage.list()) == nil then 
+                break
+            end
             farm_storage.pushItems(peripheral.getName(sticks_storage), slot)
-            print("Tranfered sticks")
+
+            print("Tranfered sticks ")
         end
     end
     
