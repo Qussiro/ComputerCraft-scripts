fbb274993143d6edd8bc71b69eb417a6a8e185ff
HappyMikki <dimazharov550@gmail.com>
2024-03-04 21:49:20 +0200

    Reworked andesite lift
    Unloading via computer instead of Create:Hoppers

--- computer/3/attach.lua
+++ computer/3/attach.lua
@@ -2,75 +2,76 @@ local i = 0
 local total_burned = 0
 local total_collected = 0
 
+function save_data()
+    local file = io.open("andesite_data.txt", "w")
+    io.output(file)
+    io.write(("%d\n%d\n%d"):format(i, total_burned, total_collected))
+    io.flush()
+    io.close(file)
+end
+
+function read_data()
+    local file, err = io.open("andesite_data.txt", "r")
+    if file ~= nil then
+        i = tonumber(file:read())
+        total_burned = tonumber(file:read())
+        total_collected = tonumber(file:read())
+        io.close(file)
+    else
+        save_data()
+    end
+end
+
+read_data()
+
 while true do
     local collected = 0
     local burned = 0
     local event, side = os.pullEvent("peripheral")
     i = i + 1
     print(("\n -=- Attached #%d -=-"):format(i))
-    local modem = peripheral.wrap("bottom")
-    local barrel = peripheral.wrap("front")
-    local x, y = 0, 0
+    local modem = peripheral.wrap("back")
+    local barrel = peripheral.wrap("right")
+    local andesite = peripheral.wrap("front")
+    local garbage = peripheral.wrap("left")
+    local x, y = term.getCursorPos()
 
-    local next = next
-    local count_a = 0
-    local count_g = 0
-
-    local prev_slot = -1
-    while true do
-        local content = barrel.list()
-        local ordered_keys = {}
-        for k in pairs(content) do 
-            table.insert(ordered_keys, k)
-        end
-        table.sort(ordered_keys)
-        local slot = ordered_keys[1]
-        local item = content[slot]
-
-        x, y = term.getCursorPos()
-
-        if item == nil then
-            break
-        end
-        
-        if slot ~= prev_slot then
-            local count = item.count
-            if prev_slot == -1 then
-                count = count + 1
-            end
-            prev_slot = slot
-            
-            if item.name == "create:andesite_cobblestone" then
-                collected = collected + count
-            else
-                burned = burned + count
-            end
-            redstone.setOutput("front", true)
-            sleep(0.5)
-        end
-
+    for slot, item in pairs(barrel.list()) do
         if item.name == "create:andesite_cobblestone" then
-            redstone.setOutput("left", true)
-            redstone.setOutput("front", false)
-            term.setCursorPos(1, y)
-            term.clearLine()
-            write(("Andesite: %d"):format(item.count))
+            local count = 0
+            while count < item.count do
+                count = count + barrel.pushItems(peripheral.getName(andesite), slot)
+                collected = collected + count
+                term.setCursorPos(1, y)
+                term.clearLine()
+                write(("Andesite: %d"):format(item.count-count))
+                total_collected = total_collected + collected
+                save_data()
+                sleep(0.5)
+            end
         else
-            redstone.setOutput("left", false)
-            redstone.setOutput("front", false)
-            term.setCursorPos(1, y)
-            term.clearLine()
-            write(("Garbage: %d"):format(item.count))
-        end 
+            local count = 0
+            while count < item.count do
+                count = count + barrel.pushItems(peripheral.getName(garbage), slot)
+                burned = burned + count
+                term.setCursorPos(1, y)
+                term.clearLine()
+                write(("Garbage: %d"):format(item.count-count))
+                total_burned = total_burned + burned
+                save_data()
+                sleep(0.5)
+            end
+        end
     end
+
     term.setCursorPos(1, y)
     term.clearLine()
 
     total_collected = total_collected + collected
     total_burned = total_burned + burned
+    save_data()
     modem.transmit(3, 0, "Empty")
     print(("\nCollected: %d\nBurned: %d"):format(collected, burned))
     print(("\nTotal collected: %d\nTotal Burned: %d"):format(total_collected,total_burned))
     print("(Going Down)")
-    redstone.setOutput("left", false)
 end
